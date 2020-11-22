cb3ecd1c2c29905f2bfec56cfaadc7bacec8f9c1
Bwlclar <73199105+Bwlclar@users.noreply.github.com>
2020-11-22 17:26:38 -0500

    Update WildifreCalculator.dart

--- wildfire_calculator/lib/WildifreCalculator.dart
+++ wildfire_calculator/lib/WildifreCalculator.dart
@@ -8,37 +8,51 @@ class WildfireCalculator {
     var v = vegetation;
 
     //if temp or humidity is in invalid range return an error
-    if (t < 1 || t > 134.1 || h < 0 || h > 100) {
-      return -1;
+    if (t < -70 || t > 130 || h < 0 || h > 100 || w<0 || w>231) {
+      return "invalid input";
     }
 
     //finds temprature regression
     var temperatureRegression =
-        .0351+.0509*(Math.log(t));
+        0.00093*t + 0.18557;
+    var oldMaxT = .30647;
+    var oldMinT = .12047;
+    var newMaxT = 10;
+    var newMinT = 0;
+ //adjust temprature Regression to 0 - 10 scale
+             temperatureRegression = 
+             (((temperatureRegression - 0ldMinT) * (newMaxT - newMinT)) / (0ldMaxT - 0ldMinT)) + newMinT;
 
-    //adjust temprature Regression to 0 - 10 scale
-    temperatureRegression =
-        (temperatureRegression - .0227) * (9.0) / (.286525 - .0227) + 1.0;
+   
 
     //finds humidity regression
     var humidityRegression =
-        0.6715+0.1095(Math.log(h));
+        -.00217*h + .35989;
+    var oldMaxH = .14289;
+    var oldMinH = .35989;
+    var newMaxH = 10;
+    var newMinH = 0;
 
     //adjusts humidity regression to 1-10 scale
     humidityRegression =
-        (humidityRegression - .2707) * (9.0) / (.6431 - .2707) + 1.0;
+        (((humidityRegression - 0ldMinH) * (newMaxH - newMinH)) / (0ldMaxH - 0ldMinH)) + newMinH;
+
 
 
     //finds windSpeed regression
     var windSpeedRegression =
-        .2103+.0253*(Math.log(t));
+      .01332*w + 0.17724;
+    var oldMaxW = 3.25416;
+    var oldMinW = .17724;
+    var newMaxW = 10;
+    var newMinW = 0;
 
     //adjust windSpeed Regression to 0 - 10 scale
     windSpeedRegression =
-        (windSpeedRegression - .0227) * (9.0) / (.286525 - .0227) + 1.0;
+        (((windSpeedRegression - 0ldMinW) * (newMaxW - newMinW)) / (0ldMaxW - 0ldMinW)) + newMinW;
 
 
     //returns fire danger on a 1 - 10 scale
-    return (temperatureRegression + humidityRegression) / 2;
+    return ((temperatureRegression + humidityRegression + windSpeed) / 3).toString();
   }
 }
